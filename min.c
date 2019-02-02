851d223b2c1ae8970bdca59f4f031295375266bb
SWATHIPRIYA16 <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-02-02 16:47:14 +0530

    Update min.c

--- min.c
+++ min.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 void main()
 {
-    int N,i,min;
+    int N,i,temp;
     scanf("%d",&N);
     int arr[N];
     for(i=0;i<N;i++)
     {
         scanf("%d",&arr[i]);
     }
+    temp=arr[0];
     for(i=0;i<=N;i++)
     {
-        if(min>arr[i])
+        if(temp>arr[i])
         {
-            min=arr[i];
+            temp=arr[i];
         }
     }
-    printf("%d",min);
+    printf("%d",temp);
 }
